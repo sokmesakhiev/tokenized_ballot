067e554f8328ca7dec4bef5eafc92a7bc09156f7
sokmesakhiev <sokmesa@gmail.com>
2022-10-09 17:15:06 +0700

    add erc20votabletoken

---
+++ contracts/ERC20VotableToken.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IMyERC20Token is IERC20 {
+    function mint(address to, uint256 amount) external;
+
+    function burnFrom(address from, uint256 amount) external;
+}
+
+contract ERC20VotableToken is Ownable {
+    uint256 public ratio;
+    uint256 public price;
+    IMyERC20Token public paymentToken;
+    uint256 public ownerPool;
+
+    constructor(
+        uint256 _ratio,
+        uint256 _price,
+        address _paymentToken
+    ) {
+        ratio = _ratio;
+        price = _price;
+        paymentToken = IMyERC20Token(_paymentToken);
+    }
+
+    function buyTokens() public payable {
+        uint256 paymentReceived = msg.value;
+        uint256 amountToBeGiven = paymentReceived / ratio;
+        paymentToken.mint(msg.sender, amountToBeGiven);
+    }
+
+    function burnTokens(uint256 amount) public {
+        paymentToken.burnFrom(msg.sender, amount);
+        uint256 amountToBeReturned = amount * ratio;
+        payable(msg.sender).transfer(amountToBeReturned);
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        require(amount <= ownerPool);
+        ownerPool -= amount;
+        paymentToken.transfer(msg.sender, amount);
+    }
+}
--- contracts/MyERC20Token.sol
+++ contracts/MyERC20Token.sol
@@ -5,11 +5,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-// contract MyERC20Votes is ERC20, AccessControl, ERC20Permit, ERC20Votes {
-contract MyERC20Votes is ERC20, AccessControl{
+contract MyERC20Token is ERC20, AccessControl{
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
   constructor() ERC20("MyToken", "MTK"){
@@ -20,8 +17,4 @@ contract MyERC20Votes is ERC20, AccessControl{
   function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
     _mint(to, amount) ;
   }
-
-  // function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-  //   super.afterTokenTransfer(from, to, amount) ;
-  // }
 }
